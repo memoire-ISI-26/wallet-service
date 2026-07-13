2b03711261f56378950ed6e542c906d230ab9e5b
Ibrahim-svmb <ibrahim.svmb@gmail.com>
2026-07-13 16:26:44 +0000

    feat: set up circuit breaker

--- src/main/java/com/financedomain/wallet/proxy/TrackingProxy.java
+++ src/main/java/com/financedomain/wallet/proxy/TrackingProxy.java
@@ -1,13 +1,14 @@
 package com.financedomain.wallet.proxy;
 
 import com.financedomain.wallet.dto.TrackingEvent;
+import com.financedomain.wallet.proxy.fallback.TrackingProxyFallback;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "tracking-service")
+@FeignClient(name = "tracking-service", fallback = TrackingProxyFallback.class)
 public interface TrackingProxy {
 
     @PostMapping("/tracking/event")
